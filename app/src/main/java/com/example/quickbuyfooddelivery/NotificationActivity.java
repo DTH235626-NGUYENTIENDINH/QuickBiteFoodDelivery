d3eb3b15a6f3e489f466b7ac3a3ba2ed918cbc8f
Orbexis <nnhatanh487@gmail.com>
2026-03-24 08:10:36 +0700

    update foo&home

--- app/src/main/java/com/example/quickbuyfooddelivery/NotificationActivity.java
+++ app/src/main/java/com/example/quickbuyfooddelivery/NotificationActivity.java
@@ -1,45 +1,22 @@
 package com.example.quickbuyfooddelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.quickbuyfooddelivery.adapters.Notification1FoodAdapter;
 import com.example.quickbuyfooddelivery.models.NotificationFood1;
 import java.util.ArrayList;
 import java.util.List;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-import java.util.List;
 
 public class NotificationActivity extends BaseActivity {
-    private RecyclerView rcvVoucher;
-    private VoucherAdapter voucherAdapter;
-    private List<Voucher> voucherList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification_voucher);
+        setContentView(R.layout.activity_notification);
         setupTaskbar(R.id.btnNotification);
-
-        rcvVoucher = findViewById(R.id.rcvVoucher);
-
-        // Tạo dữ liệu mẫu cho Voucher
-        voucherList = new ArrayList<>();
-        voucherList.add(new Voucher("Có Giảm 25% Nè!", "DEAL CỰC CHILL-HAMBURGER CÁ GIẢM 25%", R.mipmap.ic_launcher));
-        voucherList.add(new Voucher("Pizza Mua 1 Tặng 1", "Duy nhất hôm nay cho mọi loại Pizza cỡ lớn", R.mipmap.ic_launcher));
-        voucherList.add(new Voucher("Miễn Phí Vận Chuyển", "Áp dụng cho đơn hàng từ 100.000 vnđ", R.mipmap.ic_launcher));
-        voucherList.add(new Voucher("Giảm 10k Cho Nước", "Thưởng thức Pepsi/Sting lạnh giá chỉ 5k", R.mipmap.ic_launcher));
-
-        voucherAdapter = new VoucherAdapter(voucherList);
-        rcvVoucher.setLayoutManager(new LinearLayoutManager(this));
-        rcvVoucher.setAdapter(voucherAdapter);
-
-        // Nút quay lại (nếu có)
-        if (findViewById(R.id.btnBack) != null) {
-            findViewById(R.id.btnBack).setOnClickListener(v -> finish());
-        }
 
         RecyclerView rcvCapNhatDonHang = findViewById(R.id.rcvCapNhatDonHang);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -51,5 +28,21 @@ public class NotificationActivity extends BaseActivity {
                 "Đơn Pepsi-N11T02N2026_035021 đã huỷ gergre ghẻ h ehg ẻhg ẻth ẻt hẻ hử hư rh ew he ư fwe g fewf ewfeqfjukefqukfgqeofugfoqwfgofgfogo geqoig ogegroqegofrgqepofgeqi"));
         Notification1FoodAdapter adapter = new Notification1FoodAdapter(list);
         rcvCapNhatDonHang.setAdapter(adapter);
+        ImageButton btnKhuyenMai = findViewById(R.id.imgIconKhuyenMai);
+        ImageButton btnDonHang = findViewById(R.id.imgIconDonHang);
+        if (btnKhuyenMai != null)
+        {
+            btnKhuyenMai.setOnClickListener(v -> {
+                Intent intent = new Intent(NotificationActivity.this, VoucherActivity.class);
+                startActivity(intent);
+            });
+        }
+        if (btnDonHang != null)
+        {
+            btnDonHang.setOnClickListener(v -> {
+                Intent intent = new Intent(NotificationActivity.this, NotificationOrderActivity.class);
+                startActivity(intent);
+            });
+        }
     }
 }
